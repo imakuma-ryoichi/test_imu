4a64b70f4298419c034fef3a264ed52023a0bb79
imakuma-ryoichi <imakuma.ryoichi5957@mail.kyutech.jp>
2026-07-17 16:12:21 +0900

    i2c設定の追い出し設定

--- src/bno055_config.cpp
+++ src/bno055_config.cpp
@@ -1,9 +1,22 @@
 #include "imu_bno055/bno055_data.hpp"
 #include <string>
+#include <yaml-cpp/yaml.h>
+#include <stdexcept>
+
 
 BNO055Config loadConfig(const std::string& path)
 {
     BNO055Config config;
+
+    YAML::Node root = YAML::LoadFile(path);
+
+    auto bno055 = root["bno055"];
+
+    config.device =
+        bno055["i2c"]["device"].as<std::string>();
+
+    config.address =
+        bno055["i2c"]["address"].as<uint8_t>();
 
     return config;
 }
