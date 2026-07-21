e7341fb694916460898f459c08f7710152d74ae7
imakuma-ryoichi <imakuma.ryoichi5957@mail.kyutech.jp>
2026-07-21 20:39:49 +0900

    testコードの変更

--- src/imu_test.cpp
+++ src/imu_test.cpp
@@ -1,102 +1,48 @@
-#include "imu_bno055/bno055.hpp"
-#include "imu_bno055/bno055_config.hpp"
-
-#include <iostream>
-
-
-int main()
-{
-    const std::string config_path = "config.yaml";
-
-    BNO055 imu("/dev/i2c-1", 0x28);
-
-
-    if (!imu.init()) {
-        std::cerr << "IMU init failed\n";
-        return 1;
-    }
-
-
-    std::cout << "IMU initialized\n";
-
-
-    if (!imu.isCalib()) {
-        std::cerr << "Calibration is not complete\n";
-        return 1;
-    }
-
-
-    CalibrationData calib_data;
-    IMUData imu_data;
-
-
-    // IMUからキャリブレーション値取得
-    if (!imu.readCalibration(calib_data, imu_data.imu_ready)) {
-        std::cerr << "Failed to read calibration\n";
-        return 1;
-    }
-
-
-    std::cout << "Calibration read successfully\n";
-
-
-    if (!imu_data.imu_ready) {
-        std::cerr << "IMU is not ready\n";
-        return 1;
-    }
-
-
-    // YAMLへ保存
-    if (!saveConfig(config_path, calib_data)) {
-        std::cerr << "Failed to save config\n";
-        return 1;
-    }
-
-
-    std::cout << "Calibration saved\n";
-
-
-    // YAMLから読み込み
-    BNO055Config config = loadConfig(config_path);
-
-
-    // IMUへ復元
-    if (!imu.applyCalibration(config.calibration, imu_data.imu_ready)) {
-        std::cerr << "Failed to apply calibration\n";
-        return 1;
-    }
-
-
-    if (!imu_data.imu_ready) {
-        std::cerr << "IMU is not ready\n";
-        return 1;
-    }
-
-
-    // 読み書き確認
-    CalibrationData verify_data;
-
-
-    if (!imu.readCalibration(verify_data, imu_data.imu_ready)) {
-        std::cerr << "Failed to verify calibration\n";
-        return 1;
-    }
-
-
-    if (!imu_data.imu_ready) {
-        std::cerr << "IMU is not ready after verify\n";
-        return 1;
-    }
-
-
-    if (verify_data == config.calibration) {
-        std::cout << "Calibration verify success\n";
-    }
-    else {
-        std::cerr << "Calibration verify failed\n";
-        return 1;
-    }
-
-
-    return 0;
+#include "imu_bno055/bno055.hpp"
+#include "imu_bno055/bno055_config.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    BNO055 imu("/dev/i2c-1", 0x28);
+
+    if (!imu.init()) {
+        std::cerr << "IMU init failed\n";
+        return 1;
+    }
+
+    std::cout << "IMU initialized\n";
+
+    while (true) {
+        IMUData imu_data;
+
+        if (!imu.readData(imu_data)) {
+            std::cerr << "Failed to read IMU data\n";
+        } else {
+            std::cout
+                << "Acc : "
+                << imu_data.acceleration[0] << ", "
+                << imu_data.acceleration[1] << ", "
+                << imu_data.acceleration[2] << '\n';
+
+            std::cout
+                << "Gyro: "
+                << imu_data.gyroscope[0] << ", "
+                << imu_data.gyroscope[1] << ", "
+                << imu_data.gyroscope[2] << '\n';
+
+            std::cout
+                << "Quat: "
+                << imu_data.quaternion[0] << ", "
+                << imu_data.quaternion[1] << ", "
+                << imu_data.quaternion[2] << ", "
+                << imu_data.quaternion[3] << "\n\n";
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // 1秒ごと
+    }
+
+    return 0;
 }
